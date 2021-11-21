9d742dd1452646f91aa28991d886af18cb0ffb09
HAMAXx1122 <88665167+HAMAXx1122@users.noreply.github.com>
2021-11-21 08:29:43 -0800

    Update ready.js

--- src/bot/events/api/ready.js
+++ src/bot/events/api/ready.js
@@ -4,7 +4,6 @@ const figlet = require("figlet");
 module.exports = async function(client) {
     console.log(chalk.yellow.bold(figlet.textSync("jano")));
     await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold("Is Ready"));
-
-    await client.user.setActivity("Dhelp");
+await client.user.setActivity(`Dhelp | ${client.guilds.cache.size} server`);
     await client.user.setStatus("idle");
 }
