c54fa201282c894a5bb818c991cbcac4e50c270a
HAMAXx1122 <88665167+HAMAXx1122@users.noreply.github.com>
2021-11-25 23:21:18 -0800

    Update ready.js

--- src/bot/events/api/ready.js
+++ src/bot/events/api/ready.js
@@ -5,5 +5,5 @@ module.exports = async function(client) {
     console.log(chalk.yellow.bold(figlet.textSync("jano")));
     await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold("Is Ready"));
 await client.user.setActivity(`Dhelp | ${client.guilds.cache.size} server`);
-    await client.user.setStatus("idle");
+    await client.user.setStatus("online");
 }
