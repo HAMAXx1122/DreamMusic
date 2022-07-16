e12b6f58390b5fc7d3057467583cd84801e14ee9
HAMA DEV <88665167+HAMAXx1122@users.noreply.github.com>
2022-07-16 15:26:11 +0300

    Update invite.js

--- src/bot/commands/invite.js
+++ src/bot/commands/invite.js
@@ -13,9 +13,9 @@ module.exports = {
                
                 .setDescription(`
                 
-                **Invite, DreamMusic**
+                **Invite, Of ReynaBot.Thanks For Inviting Me**
                 
-  **[ Invite Me Now , thanks ](https://discord.com/api/oauth2/authorize?client_id=830265742795866162&permissions=8&scope=bot)** 
+  **[ Invite , THank ](https://discord.com/api/oauth2/authorize?client_id=873989388302946344&permissions=414530771264&scope=bot)** 
    `);
 
 
