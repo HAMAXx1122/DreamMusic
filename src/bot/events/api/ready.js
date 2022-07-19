c2e78ff403582feaf3712fe87621e61c1fab760b
HAMA DEV <88665167+HAMAXx1122@users.noreply.github.com>
2022-07-19 12:28:22 +0300

    Update ready.js

--- src/bot/events/api/ready.js
+++ src/bot/events/api/ready.js
@@ -4,6 +4,6 @@ const figlet = require("figlet");
 module.exports = async function(client) {
     console.log(chalk.yellow.bold(figlet.textSync("jano")));
     await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold("Is Ready"));
-await client.user.setActivity(`Rhelp | ${client.guilds.cache.size} Servers,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`)
+await client.user.setActivity(`Nhelp | ${client.guilds.cache.size} Servers,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`)
     await client.user.setStatus("online");
 }
