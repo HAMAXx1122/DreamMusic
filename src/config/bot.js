c3b199c311bc6929bb234b89fbd3371d297c45d3
HAMA DEV <88665167+HAMAXx1122@users.noreply.github.com>
2022-08-05 22:09:34 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    token: 'ODczOTg5Mzg4MzAyOTQ2MzQ0.GuIMXd.QkdRkdEQyoCeNhvbRxTtBdR3v0pHXd8-dTXCkI',
+    token: 'ODczOTg5Mzg4MzAyOTQ2MzQ0.G1RJrZ.PKzhu1dLLn7w3NB7rvKK7u0V3nd5dJIFSo8-2c',
     prefix: '-'
 }
